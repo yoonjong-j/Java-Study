e2d14c14b801aa6aafaba1a122ac0fa51e92350a
YoonjongJ <yoonjong.jang.123@gmail.com>
2024-09-11 01:10:52 +0900

    퀴즈 파일 생성

--- src/Quiz/WeeklyQuiz/Interface/Product.java
+++ src/Quiz/WeeklyQuiz/Interface/Product.java
@@ -1,4 +1,13 @@
 package Quiz.WeeklyQuiz.Interface;
 
 public class Product implements Promotion {
+    private String name; // 제품 이름
+    private int price; // 제품 가격
+    private int weight; // 제품 무게
+
+    Product(String name, int price, int weight) {
+        this.name = name;
+        this.price = price;
+        this.weight = weight;
+    }
 }
