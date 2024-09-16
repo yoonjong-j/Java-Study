d75e02b097b5d9f3802d12f7ddd77c1b9c409766
YoonjongJ <yoonjong.jang.123@gmail.com>
2024-09-16 13:18:22 +0900

    코드 수정

--- src/Quiz/WeeklyQuiz/Interface/Cart.java
+++ src/Quiz/WeeklyQuiz/Interface/Cart.java
@@ -2,13 +2,50 @@ package Quiz.WeeklyQuiz.Interface;
 
 public class Cart {
     private Product[] products;
+    private int totalAmount; // 상품 최종 가격
+    private int deliverycharge; // 상품 배송비
 
     Cart(Product[] products) {
         this.products = products;
+        this.totalAmount = 0;
+        this.deliverycharge = 0;
     }
 
+    // 배송비 계산
     int calculateDeliveryCharge() {
-        return 0;
+        int totalWeight = 0;
+        int totalPrice = 0;
+        int totalCharge = 0;
+
+        for (Product product : this.products) {
+            totalWeight += product.getWeight();
+        }
+
+        for (Product product : this.products) {
+            totalPrice += product.getPrice();
+        }
+
+        if (totalWeight < 3) {
+            totalCharge = 1000;
+        }
+        else if (totalWeight >= 3 && totalWeight < 10) {
+            totalCharge = 5000;
+        }
+        else {
+            totalCharge = 10_000;
+        }
+
+        if (totalPrice < 30_000) {
+            totalCharge = totalCharge;
+        }
+        else if (totalPrice >= 30_000 && totalPrice < 100_000) {
+            totalCharge -= 1000;
+        }
+        else {
+            totalCharge = 0;
+        }
+
+        return totalCharge;
     }
 
     public static void main(String[] args) {
